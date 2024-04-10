8bb01544333b53a51138f63802c845960eb9234b
Кирилл Никитин <kirnik2003@mail.ru>
2024-04-10 14:01:02 +0700

    Add Matrix Analyzer functional

---
+++ Lab-1/Controllers/MatrixController.cs
@@ -0,0 +1,29 @@
+﻿using Lab_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class MatrixController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Analyze(int rows, int columns)
+        {
+            if (rows <= 0 || columns <= 0)
+            {
+                ViewBag.Result = "Invalid matrix size.";
+                return View("Index");
+            }
+
+            MatrixModel model = new MatrixModel();
+            model.GenerateMatrix(rows, columns);
+            model.AnalyzeMatrix();
+
+            return View("Results", model);
+        }
+    }
+}
---
+++ Lab-1/Models/MatrixModel.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab_1.Models
+{
+    public class MatrixModel
+    {
+        public int[,] Matrix { get; set; }
+        public int[] MinInColumns { get; set; }
+        public int[] MaxInRows { get; set; }
+
+        public void GenerateMatrix(int rows, int columns)
+        {
+            Random random = new Random();
+            Matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Matrix[i, j] = random.Next(1, 101); // случайные числа от 1 до 100
+                }
+            }
+        }
+
+        public void AnalyzeMatrix()
+        {
+            int rows = Matrix.GetLength(0);
+            int columns = Matrix.GetLength(1);
+
+            MinInColumns = new int[columns];
+            MaxInRows = new int[rows];
+
+            for (int j = 0; j < columns; j++)
+            {
+                int min = Matrix[0, j];
+                for (int i = 1; i < rows; i++)
+                {
+                    if (Matrix[i, j] < min)
+                    {
+                        min = Matrix[i, j];
+                    }
+                }
+                MinInColumns[j] = min;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                int max = Matrix[i, 0];
+                for (int j = 1; j < columns; j++)
+                {
+                    if (Matrix[i, j] > max)
+                    {
+                        max = Matrix[i, j];
+                    }
+                }
+                MaxInRows[i] = max;
+            }
+        }
+    }
+}
