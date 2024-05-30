b2424dd95cdd8ed8420011c23b54ebfe5f813f44
Кирилл Никитин <kirnik2003@mail.ru>
2024-05-30 23:47:37 +0700

    Lab 2 done

---
+++ Lab-2/Models/CoreModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab_2.Models
+{
+    public class CoreModel
+    {
+        public List<Group> Groups { get; set; } = new List<Group>();
+        public Student NewStudent { get; set; } = new Student();
+        public bool ShowGroups { get; set; } = false;
+        public Group CurrentGroup { get; set; } = new Group();
+        public string Message { get; set; } = "";
+
+        public CoreModel(List<Group> groups)
+        {
+            Groups = groups;
+        }
+
+        public CoreModel()
+        {
+        }
+    }
+}
